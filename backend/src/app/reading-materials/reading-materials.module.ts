df480a447abc33c39198a7d2938b68008871f765
chrlskrt <charlene.repuesto@cit.edu>
2025-08-25 15:20:04 +0800

    refactor: update reading materials module

--- backend/src/app/reading-materials/reading-materials.module.ts
+++ backend/src/app/reading-materials/reading-materials.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReadingMaterialsService } from './reading-materials.service';
 import { ReadingMaterialsController } from './reading-materials.controller';
+import { GenresService } from '../genres/genres.service';
+import { ReadabilityService } from './readibility.service';
 
 @Module({
   controllers: [ReadingMaterialsController],
-  providers: [ReadingMaterialsService],
+  providers: [ReadingMaterialsService, ReadabilityService, GenresService],
 })
 export class ReadingMaterialsModule {}
