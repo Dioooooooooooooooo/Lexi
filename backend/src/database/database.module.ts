347828d2966398e71ad40dde94924c35988e9789
Kuugang <jakebajo11@gmail.com>
2025-09-26 17:58:09 +0800

    chore: db ssl

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,21 +1,27 @@
-import { Global, Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { Kysely, PostgresDialect } from "kysely";
-import { Pool } from "pg";
-import { DatabaseService } from "./database.service";
-import { DB } from "./db";
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Kysely, PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
+import { DatabaseService } from './database.service';
+import { DB } from './db';
 
 @Global()
 @Module({
   providers: [
     {
-      provide: "DATABASE",
+      provide: 'DATABASE',
       useFactory: (configService: ConfigService) => {
-        const connectionString = configService.get<string>("DATABASE_URL");
         const dialect = new PostgresDialect({
           pool: new Pool({
-            connectionString,
-            ssl: { rejectUnauthorized: false },
+            host: configService.get<string>('DB_HOST'),
+            port: parseInt(configService.get<string>('DB_PORT') ?? '5432', 10),
+            user: configService.get<string>('DB_USERNAME'),
+            password: configService.get<string>('DB_PASSWORD'),
+            database: configService.get<string>('DB_NAME'),
+            ssl: {
+              rejectUnauthorized: true,
+              ca: configService.get<string>('DB_SSL_CA'),
+            },
           }),
         });
         return new Kysely<DB>({ dialect });
@@ -24,6 +30,6 @@ import { DB } from "./db";
     },
     DatabaseService,
   ],
-  exports: ["DATABASE", DatabaseService],
+  exports: ['DATABASE', DatabaseService],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
